45cba961b9f264699ce08473d61d3d67968d414b
rightpair <tkddn204@gmail.com>
2023-12-22 18:43:17 +0900

    :wrench:service-dev.yaml 추가

--- clouds/config-server/src/main/java/net/rightpair/ConfigServerApplication.java
+++ clouds/config-server/src/main/java/net/rightpair/ConfigServerApplication.java
@@ -1,6 +1,7 @@
 package net.rightpair;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -12,6 +13,6 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigServer
 public class ConfigServerApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
