0d4056ad7cc0b1da31a3ea648e9a0a0a4eadb718
rightpair <tkddn204@gmail.com>
2023-12-22 18:31:37 +0900

    :sparkles: config-server 모듈 추가

---
+++ clouds/config-server/src/main/java/net/rightpair/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package net.rightpair;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+
+    }
+}
